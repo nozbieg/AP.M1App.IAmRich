ba46d6e35eb851441a2c6a53f516efc240b4b3ea
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-11-20 11:44:04 +0100

    Add project files.

---
+++ AP.M1App.IAmRich/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Linq;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls.Hosting;
+global using Microsoft.Maui.Hosting;
+
---
+++ AP.M1App.IAmRich/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace AP.M1App.IAmRich
+{
+    public partial class MainPage : ContentPage
+    {
+        bool rich = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnRichStateClicked(object sender, EventArgs e)
+        {
+            if (rich)
+            {
+                RichState.Text = $"You are: poor";
+                rich = false;
+                RichStateButton.Text = "Click to get rich!";
+
+
+            }
+            else
+            {
+                RichState.Text = $"You are: rich";
+                rich = true;
+                RichStateButton.Text = "Click to be poor again!";
+            }
+
+            SemanticScreenReader.Announce(RichState.Text);
+        }
+    }
+}
