6c0fce982f0163da7d808a1ac7033c42f5771322
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-11-20 14:21:27 +0100

    .

--- AP.M1App.IAmRich/MainPage.xaml.cs
+++ AP.M1App.IAmRich/MainPage.xaml.cs
@@ -2,35 +2,35 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
-namespace AP.M1App.IAmRich
+namespace AP.M1App.IAmRich;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    bool rich = false;
+
+    public MainPage()
     {
-        bool rich = false;
+        InitializeComponent();
+    }
 
-        public MainPage()
+    void OnRichStateClicked(object sender, EventArgs e)
+    {
+        if (rich)
         {
-            InitializeComponent();
+            RichState.Text = $"You are: poor";
+            rich = false;
+            RichStateButton.Text = "Click to get rich!";
+
+
+        }
+        else
+        {
+            RichState.Text = $"You are: rich";
+            rich = true;
+            RichStateButton.Text = "Click to be poor again!";
         }
 
-        void OnRichStateClicked(object sender, EventArgs e)
-        {
-            if (rich)
-            {
-                RichState.Text = $"You are: poor";
-                rich = false;
-                RichStateButton.Text = "Click to get rich!";
-
-
-            }
-            else
-            {
-                RichState.Text = $"You are: rich";
-                rich = true;
-                RichStateButton.Text = "Click to be poor again!";
-            }
-
-            SemanticScreenReader.Announce(RichState.Text);
-        }
+        SemanticScreenReader.Announce(RichState.Text);
     }
 }
+
